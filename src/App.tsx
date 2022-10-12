a61fc55d0400fa471836706ab5763e10d7fb57cc
Mauro <mauro.roda@hotmail.com>
2022-10-12 18:21:13 +0100

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+interface QuoteData {
+  _id?: String
+  content: String
+  author: String
+  authorSlug?: String
+  length?: Number
+  tags?: String[]
+}
+
 function App() {
+  const [data, setData] = useState<QuoteData>();
+
+  async function updateQuote() {
+    let response;
+    try {
+      response = await fetch("https://api.quotable.io/random");
+      const { statusCode, statusMessage, ...data } = await response.json();
+      if(!response.ok) throw new Error (`${statusCode} ${statusMessage}`);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+      setData({ content: 'Oops... something went wrong.', author: ''});
+    }
+  }
+
+  // async function updateQuote(): Promise<ResponseData[]> {
+  //   return fetch('https://api.quotable.io/random').then(res => res.json()).then(res => {
+  //     return res as ResponseData[]
+  //   })
+  // }
+
+  useEffect(() => {
+    updateQuote();
+  }, []);
+
+  if (!data) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+        <p className='quote'>"{data.content}"</p>
+        <p className='author'> -{data.author}</p>
+      </div>
     </div>
   );
 }
